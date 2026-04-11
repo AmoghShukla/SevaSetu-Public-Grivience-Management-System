2fd707debdea1731f56071a106424b17454d100d
Amogh Shukla <amoghshukla548@gmail.com>
2026-04-12 00:12:21 +0530

    feat: add alembic for database migrations and pydantic-settings for configuration management
    - Added alembic (1.18.4) for database migration support.
    - Introduced pydantic-settings (2.13.1) for enhanced settings management.
    - Updated pyproject.toml to include new dependencies: alembic and pydantic-settings.
    - Created alembic.ini for alembic configuration.
    - Implemented env.py for migration environment setup.
    - Added script.py.mako for migration script template.
    - Developed config.py to manage application settings using pydantic.
    - Included README in alembic directory for documentation.

---
+++ config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DB_PORT: int
+    DB_HOST: str
+    DB_USER: str
+    DB_NAME: str
+    DB_PASSWORD: str
+
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    class Config:
+            env_file = ".env"
+
+
+settings = Settings()
