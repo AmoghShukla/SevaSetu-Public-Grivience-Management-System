d11abadf067f2b00742f20329d6cf75e7344f8a5
Amogh Shukla <amoghshukla548@gmail.com>
2026-04-24 23:20:01 +0530

    feat: ensure log directory exists before logging setup

--- src/utlis/logger.py
+++ src/utlis/logger.py
@@ -4,4 +4,10 @@ import os
 def get_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(level=logging.INFO)
+
+    if not logger.hasHandlers():
+        if not os.path.exists('src/logs'):
+            os.mkdir('src/logs')
+
+            
     
