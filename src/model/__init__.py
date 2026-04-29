a4bbb5dfad2b0afcd0bd73de4f09250776f3058c
Amogh Shukla <amoghshukla548@gmail.com>
2026-04-29 23:42:51 +0530

    feat: import Grivience_Class in model package initialization

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,4 +1,4 @@
 from src.model.assignment import Assignment_Class
 from src.model.department import Department_class 
 from src.model.feedback import Feedback_class
-
+from src.model.grivience import Grivience_Class
