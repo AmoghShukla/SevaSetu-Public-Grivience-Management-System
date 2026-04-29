440223a082c2cb1c26619f9b3dc29d06aa79e63c
Amogh Shukla <amoghshukla548@gmail.com>
2026-04-29 23:43:50 +0530

    feat: update __all__ in model package to include Assignment_Class

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -3,3 +3,7 @@ from src.model.department import Department_class
 from src.model.feedback import Feedback_class
 from src.model.grivience import Grivience_Class
 from src.model.user import User_Class
+
+__all__ = [
+    Assignment_Class
+]
