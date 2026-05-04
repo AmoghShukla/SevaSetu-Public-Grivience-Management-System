66bc8319c232a5af9ba932fb604b0ec1c0e9cdd5
Amogh Shukla <amoghshukla548@gmail.com>
2026-05-04 23:23:12 +0530

    feat: add feedback_id column to Feedback_Class in feedback.py

--- src/model/feedback.py
+++ src/model/feedback.py
@@ -1,6 +1,8 @@
 from src.database.base import Base
-from sqlalchemy import column
+from sqlalchemy import UUID, column
 
 class Feedback_Class(Base):
     __tablename__="Feedback_Table"
+
+    feedback_id = column(UUID, primary_key = True, index = True)
 
