2ad3201f2f03644b04a0009dc3ca34288376e627
Amogh Shukla <amoghshukla548@gmail.com>
2026-05-04 23:25:54 +0530

    feat: add ForeignKey relationship for grivience_id in Feedback_Class

--- src/model/feedback.py
+++ src/model/feedback.py
@@ -1,8 +1,9 @@
 from src.database.base import Base
-from sqlalchemy import UUID, column
+from sqlalchemy import UUID, ForeignKey, column
 
 class Feedback_Class(Base):
     __tablename__="Feedback_Table"
 
     feedback_id = column(UUID(as_uuid=True), primary_key = True, index = True)
+    grivience_id = column(UUID(as_uuid=True), ForeignKey('Grivience_Table.grivience_id'))
 
