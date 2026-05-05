1db88eff39f74a10671c945f5fa1bb4e92b04b74
Amogh Shukla <amoghshukla548@gmail.com>
2026-05-05 22:04:27 +0530

    feat: update department_id to use UUID type in Department_class

--- src/model/department.py
+++ src/model/department.py
@@ -1,9 +1,11 @@
+from uuid import UUID
+
 from src.database.base import Base
 from sqlalchemy import column
 
 class Department_class(Base):
     __tablename__="Department"
 
-    department_id = column(...,nullable=False)
+    department_id = column(UUID(as_UUID=True),nullable=False)
     department_name = column(...,nullable=False)
     department_description = column(...,nullable=False)
