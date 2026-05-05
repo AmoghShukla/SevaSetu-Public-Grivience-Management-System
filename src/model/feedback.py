886a53e730a735a7d21ddbef06af5da547b68b25
Amogh Shukla <amoghshukla548@gmail.com>
2026-05-05 21:43:24 +0530

    feat: added relationship for grivience in Feedback_Class

--- src/model/feedback.py
+++ src/model/feedback.py
@@ -1,3 +1,5 @@
+from sqlalchemy.orm import Relationship
+
 from src.database.base import Base
 from sqlalchemy import UUID, ForeignKey, column
 
@@ -6,4 +8,6 @@ class Feedback_Class(Base):
 
     feedback_id = column(UUID(as_uuid=True), primary_key = True, index = True)
     grivience_id = column(UUID(as_uuid=True), ForeignKey('Grivience_Table.grivience_id'))
+
+    grivience = Relationship('Grivience_Class', back_populates='feedback')
 
