29a16af67bd0f7d07789fb3bda1dd3a15211c427
Amogh Shukla <amoghshukla548@gmail.com>
2026-05-05 22:05:48 +0530

    feat: update department_id and grivience_id to use UUID type with default value

--- src/model/department.py
+++ src/model/department.py
@@ -1,4 +1,4 @@
-from uuid import UUID
+from uuid import UUID, uuid4
 
 from src.database.base import Base
 from sqlalchemy import column
@@ -6,6 +6,6 @@ from sqlalchemy import column
 class Department_class(Base):
     __tablename__="Department"
 
-    department_id = column(UUID(as_UUID=True),nullable=False)
+    department_id = column(UUID(as_UUID=True), default=uuid4,nullable=False)
     department_name = column(...,nullable=False)
     department_description = column(...,nullable=False)
--- src/model/grivience.py
+++ src/model/grivience.py
@@ -1,3 +1,5 @@
+from uuid import UUID, uuid4
+
 from src.database.base import Base
 from sqlalchemy import column, Integer, String, ForeignKey, Text
 from sqlalchemy.orm import relationship
@@ -5,7 +7,7 @@ from sqlalchemy.orm import relationship
 class Grivience_Class(Base):
     __tablename__="Grivience"
 
-    grivience_id = column(Integer, primary_key=True, index=True)
+    grivience_id = column(UUID(as_UUID=True), default=uuid4, primary_key=True, index=True)
     grivience_title = column(String, nullable=False)
     grivience_description = column(Text, nullable=False)
 
