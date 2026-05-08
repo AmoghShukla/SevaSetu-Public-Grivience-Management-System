33c48f5699b671c3ec6be3014454fbab7d5d329c
Amogh Shukla <amoghshukla548@gmail.com>
2026-05-08 23:15:43 +0530

    fix: corrected formatting string in logger setup

--- src/utlis/logger.py
+++ src/utlis/logger.py
@@ -9,7 +9,7 @@ def get_logger(name):
         if not os.path.exists('src/logs'):
             os.mkdir('src/logs/app.log')
 
-        formatter = logging.Formatter('  %(asctime)s | %(levelname)s | %(name)s | %(message)s')
+        formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(name)s | %(message)s')
 
         ConsoleHandler = logging.StreamHandler()
         ConsoleHandler.setFormatter(formatter)
