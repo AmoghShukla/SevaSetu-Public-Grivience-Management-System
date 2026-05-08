5dc175f781c14226c0d5521c392d33870f628340
Amogh Shukla <amoghshukla548@gmail.com>
2026-05-08 23:07:52 +0530

    fix: corrected logging setup in get_logger function and add logger info in home endpoint

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+from src.utlis.logger import get_logger
+
+logger = get_logger(__name__)
 
 app = FastAPI(title="SevaSetu : Public Grivience Resolution System")
 
 @app.get('/')
 def home():
+    logger.info("The Application is running")
     return {
         'message' : 'The Application is up and running!!!'
     }
--- src/utlis/logger.py
+++ src/utlis/logger.py
@@ -6,15 +6,15 @@ def get_logger(name):
     logger.setLevel(level=logging.INFO)
 
     if not logger.hasHandlers():
-        if not os.path.exists('src/logs/app/log'):
-            os.makedirs('src/logs/app.log')
+        if not os.path.exists('src/logs'):
+            os.mkdir('src/logs/app.log')
 
-        formatter = logging.Formatter('  %(asctimes)s | %(levelname)s | %(name)s | %(message)s')
+        formatter = logging.Formatter('  %(asctime)s | %(levelname)s | %(name)s | %(message)s')
 
         ConsoleHandler = logging.StreamHandler()
         ConsoleHandler.setFormatter(formatter)
 
-        FileHandler = logging.FileHandler()
+        FileHandler = logging.FileHandler('src/logs/app.log')
         FileHandler.setFormatter(formatter)
 
         logger.addHandler(ConsoleHandler)
