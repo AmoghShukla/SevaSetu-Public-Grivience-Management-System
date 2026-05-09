09f14c46b57581e4cafac5e1df06a6b2355c1044
Amogh Shukla <amoghshukla548@gmail.com>
2026-05-09 21:43:05 +0530

    Refactoredd Get_Feedback_By_ID method to use Feedback_Class and update parameter name

--- src/repository/feedback.py
+++ src/repository/feedback.py
@@ -2,7 +2,7 @@ from sqlalchemy import select
 from sqlalchemy.exc import SQLAlchemyError
 
 from src.exception import CustomException
-from src.model.user import User_Class
+from src.model.feedback import Feedback_Class
 
 
 class FeedbackRepository:
@@ -12,11 +12,11 @@ class FeedbackRepository:
         pass
 
     @staticmethod
-    def Get_Feedback_By_ID(user_id, db):
+    def Get_Feedback_By_ID(feedback_id, db):
         try:
             return db.execute(
-                select(User_Class).
-                where(User_Class.user_id == user_id)
+                select(Feedback_Class).
+                where(Feedback_Class.feedback_id == feedback_id)
                 .scalars()
                 .first()
                 )
