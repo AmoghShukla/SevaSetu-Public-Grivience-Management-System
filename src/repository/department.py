e2c3ee189a647e6ea256ddc31c035dccb7cd27ec
Amogh Shukla <amoghshukla548@gmail.com>
2026-05-10 22:20:11 +0530

    Implemented Hard_Delete_Department method with error handling and success message

--- src/repository/department.py
+++ src/repository/department.py
@@ -1,5 +1,21 @@
+from sqlalchemy.exc import SQLAlchemyError
+
+from src.exception import CustomException
+
+
 class DepartmentRepository:
 
     @staticmethod
     def Create_Department():
         pass
+    
+    @staticmethod
+    def Hard_Delete_Department(department, db):
+        try:
+            db.delete(department)
+            db.commit()
+            return {
+                'message' : 'Department Deleted!!!'
+            }
+        except SQLAlchemyError as e:
+            raise CustomException.RepositoryError(e) from e
