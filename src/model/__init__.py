1e42a049ab91b28fb16ae6e5cfbab789fd13695b
Amogh Shukla <amoghshukla548@gmail.com>
2026-05-13 23:28:23 +0530

    Fix casing of Feedback_Class import in model __init__.py

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,13 +1,13 @@
 from src.model.assignment import Assignment_Class
 from src.model.department import Department_class 
-from src.model.feedback import Feedback_class
+from src.model.feedback import Feedback_Class
 from src.model.grivience import Grivience_Class
 from src.model.user import User_Class
 
 __all__ = [
     Assignment_Class,
     Department_class,
-    Feedback_class,
+    Feedback_Class,
     Grivience_Class,
     User_Class
 ]
