3da6fd9d29338692625f05b9d6e4845b63df04c7
Amogh Shukla <amoghshukla548@gmail.com>
2026-05-16 11:12:10 +0530

    Refactored UserCreate model in user.py to improve structure and add optional fields in UserResponse and UserUpdate models

--- src/schema/user.py
+++ src/schema/user.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseModel, EmailStr
 
 class UserCreate(BaseModel):
@@ -5,3 +6,13 @@ class UserCreate(BaseModel):
     user_email : EmailStr
     user_password : str
     user_contact_no : str
+
+class UserResponse(BaseModel):
+    user_name : Optional[str]
+    user_email : Optional[EmailStr]
+    user_contact_no : Optional[str]
+
+class UserUpdate(BaseModel):
+    user_name : Optional[str] = None
+    user_email : Optional[EmailStr] = None
+    user_contact_no : Optional[str] = None
